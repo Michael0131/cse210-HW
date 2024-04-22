a4ca35d9c51ebafdb3a504e3b375d6da9b16890b
Michael0131 <michaelj0131@byui.edu>
2024-04-22 16:25:37 -0600

    This is a test

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello world this is a test to see if the clone worked");
     }
 }
