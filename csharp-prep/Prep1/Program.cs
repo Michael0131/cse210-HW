2b3b8393b7454c138a3c7d223b5cadf1ca3a8dfd
Michael0131 <michaelj0131@byui.edu>
2024-04-24 18:22:40 -0600

    prep_one_project_Complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        
+        // get the first name
+        // create the prompt for user to input
+        Console.Write("enter your first name:");
+        //have C# capture the input, and assign it a variable
+        string first_name= Console.ReadLine();
+        //capture the last name
+        Console.Write("Enter your last name:");
+        // have C# capture the last name
+        string last_name = Console.ReadLine();
+
+        // create print function to introduce user like bond
+        Console.WriteLine($"Your name is {last_name}, {first_name}  {last_name} ");
+
+
+
+
     }
 }
