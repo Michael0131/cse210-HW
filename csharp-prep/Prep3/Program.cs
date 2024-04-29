9051a480f6bbacb8cc9d38e0df42353f7d9a326d
MichaeJ0131 <91762833+MichaeJ0131@users.noreply.github.com>
2024-04-29 10:42:05 -0600

    Prep3 Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+    
+        int number_guess = 0;
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(0, 100);
+        while (number_guess !=  number)
+        {
+            Console.Write("What is your guess? ");
+            number_guess =int.Parse(Console.ReadLine());
+            //number_guess = Console.ReadLine();
+            if (number_guess > number)
+            {
+                Console.WriteLine("Too High - Go Lower");
+            }
+            else if (number_guess < number)
+            {
+                Console.WriteLine("Too Low - Think Higher");
+            }
+            else
+            {
+                Console.WriteLine("You are correct, the number is " + number + "!");
+            }
+        }
+    
     }
+
 }
+
