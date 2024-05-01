cbee7433309cafd4f675ddcb2302a2d6b5f5521f
MichaeJ0131 <91762833+MichaeJ0131@users.noreply.github.com>
2024-05-01 10:02:41 -0600

    finished most of the assignmnent sum and list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,12 +1,58 @@
 using System;
 using Microsoft.VisualBasic;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int user_input = -1;
+        //string user_variable = "Hello";
+        int List_size = 0;
+
+        Console.WriteLine("Enter  alist of numbers, type '0' when you are done. ");
+        while (user_input != 0)
+        {
+            Console.Write("Enter a number:");
+            
+            string user_response = Console.ReadLine();
+            user_input = int.Parse(user_response);
+            
+        
+            if (user_input != 0)
+            {
+                numbers.Add(user_input);
+            }
+
+        }
+        List_size = numbers.Count;
+        int sum =0;
+
+        foreach (int number in numbers)
+        {
+            
+            sum += number;
+
+    
+        }
+        Console.WriteLine("The sum is: " + sum);
+        Console.WriteLine($"The average is: " + sum/List_size);
+        // Find largest
+        int max = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine("The Largest number is: " + max);
+        
+
 
 
         
