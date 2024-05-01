6ba3e8f496c4ad63fff960a27ea33af67f685a30
MichaeJ0131 <91762833+MichaeJ0131@users.noreply.github.com>
2024-05-01 09:16:28 -0600

    did a stretch challange

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,26 +7,31 @@ class Program
     static void Main(string[] args)
 
     {
+        int guess_count = 0;
     
-        int number_guess = 0;
+        int number_guess = -1;
         Random randomGenerator = new Random();
         int number = randomGenerator.Next(0, 100);
         while (number_guess !=  number)
         {
+            guess_count += 1;
             Console.Write("What is your guess? ");
             number_guess =int.Parse(Console.ReadLine());
             //number_guess = Console.ReadLine();
+            
             if (number_guess > number)
             {
-                Console.WriteLine("Too High - Go Lower");
+                Console.WriteLine("Too High - Guess Lower");
+                
             }
             else if (number_guess < number)
             {
-                Console.WriteLine("Too Low - Think Higher");
+                Console.WriteLine("Too Low - Guess Higher");
             }
             else
             {
                 Console.WriteLine("You are correct, the number is " + number + "!");
+                Console.WriteLine("Congratulations, it took you " + guess_count +" guesses.");
             }
         }
     
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,10 +1,14 @@
 using System;
+using Microsoft.VisualBasic;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+
         
     }
 }
