072a4555db7c2b9efd97b00e4afd8f8bed5fc711
MichaeJ0131 <91762833+MichaeJ0131@users.noreply.github.com>
2024-05-09 22:49:26 -0600

    jobs and resume files made

---
+++ prepare/Learning02/Jobs.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Job
+{
+    public string _jobtitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void display()
+
+    {
+        Console.WriteLine($"{_jobtitle} ({_company}) {_startYear} {_endYear}" );
+
+
+    }
+
+
+
+}
